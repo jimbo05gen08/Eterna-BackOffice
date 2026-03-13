bab5c5fcf34148d29c8ffb72f4bf0a186f529ac7
jimbo05gen08 <jimmy.obregon@3ek.pe>
2026-03-13 16:02:03 -0500

    imagen

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -10,8 +10,12 @@ import { RevenueStreamWidget } from "./components/revenuestreamwidget";
       style="display:flex;justify-content:center;align-items:center;width:100%; height: 600px;position:relative"
     >
       <div style="position: absolute;top:100px;text-align:center">
-        <h3>Bienvenido a Eterna BackOffice</h3>
-        <img src="../../../assets/eterna-assets/logo_verde.svg" alt="" />
+        <h3 style="color: white;">Bienvenido a Eterna BackOffice</h3>
+        <img
+          src="../../../assets/eterna-assets/logo_verde.svg"
+          alt=""
+          style="margin-left: 70px;"
+        />
       </div>
       <img
         src="../../../assets/eterna-assets/Frame 20-3.png"
